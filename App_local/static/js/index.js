d197c40eb7503f27606bb1c3bea57658186dcac7
Josh <joshuakuminski.nysdot@gmail.com>
2023-01-27 14:32:10 -0500

    In Progress

--- App_local/static/js/index.js
+++ App_local/static/js/index.js
@@ -28,11 +28,11 @@ function openFile() {
 
 //set the button id to a variable 'addingLineBtn'
 let addingLineBtn = document.getElementById("adding-line-btn");
-
+let addingMaskBtn = $("#mask-btn")[0];
 
 //Call the function 'activateAddingLine' When button is pressed
 addingLineBtn.addEventListener('click', activateAddingLine);
-
+addingMaskBtn.addEventListener('click', activateAddingMask);
 
 //create random int
 function getRandomInt(max) {
@@ -55,6 +55,10 @@ function activateAddingLine(){
     canvas.on('mouse:down', startAddingLine);
     canvas.on('mouse:move', startDrawingLine);
     canvas.on('mouse:up', stopDrawingLine);
+
+    canvas.off('mouse:down', startAddingRect);
+    canvas.off('mouse:move', startDrawingRect);
+    canvas.off('mouse:up', stopDrawingRect);
 
     canvas.selection = false;
 }
@@ -176,6 +180,74 @@ function stopDrawingLine(o){
         ddID[i].options.add(option);
     }
 
+}
+
+function activateAddingMask(){
+    //clear out the list if button is not disabled
+    if (addingMaskBtn.disabled === false){
+        console.log('refresh list');
+        //send_coords_toFlask(0, 0, 3, 0);
+    }
+
+    //add a disable class when button is click so it cant be clicked a second time
+    $('.mask_btn').addClass('disabled');
+
+    //Add event listeners
+    canvas.on('mouse:down', startAddingRect);
+    canvas.on('mouse:move', startDrawingRect);
+    canvas.on('mouse:up', stopDrawingRect);
+
+    canvas.off('mouse:down', startAddingLine);
+    canvas.off('mouse:move', startDrawingLine);
+    canvas.off('mouse:up', stopDrawingLine);
+
+    canvas.selection = false;
+}
+
+function startAddingRect(o){
+    mouseDown = true;
+    click_event = 3;
+    let  pointer = canvas.getPointer(o.e);
+
+    origX = 0
+    origY = 0
+
+    rect = new fabric.Rect({
+        left: origX,
+        top: origY,
+        originX: 'left',
+        originY: 'top',
+        width: pointer.x - origX,
+        height: pointer.y - origY,
+        angle: 0,
+        fill: 'rgba(255,0,0,0.5)',
+        transparentCorners: false
+    });
+
+    canvas.add(rect);
+    canvas.requestRenderAll();
+    //send_coords_toFlask(pointer.x, pointer.y, click_event);
+}
+
+function startDrawingRect(o){
+    if (mouseDown === true){
+        let  pointer = canvas.getPointer(o.e);
+
+        rect.set({
+            width: pointer.x - origX,
+            height: pointer.y - origY
+        });
+
+        canvas.requestRenderAll();
+    }
+
+}
+
+function stopDrawingRect(o){
+    let  pointer = canvas.getPointer(o.e);
+    click_event = 3;
+    mouseDown = false;
+    send_coords_toFlask(pointer.x, pointer.y, click_event);
 }
 
 let csvContent = "data:text/csv;charset=utf-8,";
